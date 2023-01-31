9108fd4483aec11e75ad0644a5a7d2499393a4f3
Benjamin Nolan (TwoWholeWorms) <ben.nolan@gmail.com>
2023-01-31 17:05:42 +0100

    fix: new GitHub -> getOctokit

--- src/create-release.js
+++ src/create-release.js
@@ -1,11 +1,11 @@
 const core = require('@actions/core');
-const { GitHub, context } = require('@actions/github');
+const { getOctokit, context } = require('@actions/github');
 const fs = require('fs');
 
 async function run() {
   try {
     // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
-    const github = new GitHub(process.env.GITHUB_TOKEN);
+    const github = getOctokit(process.env.GITHUB_TOKEN);
 
     // Get owner and repo from context of payload that triggered the action
     const { owner: currentOwner, repo: currentRepo } = context.repo;
