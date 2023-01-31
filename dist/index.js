bfd61410ca4a70719cbb62726a4fc229c789b024
Benjamin Nolan (TwoWholeWorms) <ben.nolan@gmail.com>
2023-01-31 17:40:17 +0100

    chore(release): 2.0.4

--- dist/index.js
+++ dist/index.js
@@ -9601,13 +9601,13 @@ function wrappy (fn, cb) {
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
 const core = __nccwpck_require__(2186);
-const { GitHub, context } = __nccwpck_require__(5438);
+const { getOctokit, context } = __nccwpck_require__(5438);
 const fs = __nccwpck_require__(7147);
 
 async function run() {
   try {
     // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
-    const github = new GitHub(process.env.GITHUB_TOKEN);
+    const github = getOctokit(process.env.GITHUB_TOKEN);
 
     // Get owner and repo from context of payload that triggered the action
     const { owner: currentOwner, repo: currentRepo } = context.repo;
